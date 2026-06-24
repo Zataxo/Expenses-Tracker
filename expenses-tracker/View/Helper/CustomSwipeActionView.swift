acf1ac3db5686498bef6e5f08a260c2d12f9714a
Zataxo <hassan.og@outlook.com>
2026-06-24 15:17:47 +0300

    finalize home data custom views

---
+++ expenses-tracker/View/Helper/CustomSwipeActionView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSwipeActionView.swift
+//  expenses-tracker
+//
+//  Created by Hassan Abdalla on 09/01/1448 AH.
+//
+
+import SwiftUI
+
+struct CustomSwipeActionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSwipeActionView()
+}
---
+++ expenses-tracker/View/Helper/DateFilterView.swift
@@ -0,0 +1,4 @@
+ if showFilterView {
+                    DateFilterView()
+                        .tra
+                }
